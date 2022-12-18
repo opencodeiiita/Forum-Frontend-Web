c93910c46997e82638ae9af3817f10206510db6f
Kali-Decoder <neerajchoubisa876@gmail.com>
2022-12-18 22:36:21 +0530

    Adding: Signup Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from "react";
+
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-center">
-      Forum!
-    </h1>
+      <h1 className="text-3xl font-bold underline text-center">Forum!</h1>
     </>
   );
 }
