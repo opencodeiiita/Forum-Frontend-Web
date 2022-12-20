cb4b253013438b3d4ca1f8b4e67353f44a74ee51
Ananya <goyalananya444@gmail.com>
2022-12-20 22:48:26 +0530

    navbar-task-completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from "./pages/signup/index";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">Forum!</h1>
+<>
+<Navbar />
+    <BrowserRouter>
+     <Routes>
+        <Route exact path='/Home' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+
+      <h1 className="text-3xl font-bold underline text-center mt-5">Forum!</h1>
       <Signup/>
+      
     </>
   );
 }
