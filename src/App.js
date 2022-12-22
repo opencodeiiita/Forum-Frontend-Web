080fbeabdf3cb5160a7ec5f0705a2855aa038830
ananyaa07 <97439364+ananyaa07@users.noreply.github.com>
2022-12-23 03:54:18 +0530

    Designed dashboard for forum website (#32)
    * navbar-task-completed
    
    * Update Navbar.js
    
    * task-done
    
    Co-authored-by: Adi-R12 <97426655+Adi-R12@users.noreply.github.com>
    Co-authored-by: Yash Gupta <gyash6696@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Signup from "./pages/signup/index";
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+<<<<<<< HEAD
+=======
 import Hero from './components/hero-section';
+>>>>>>> 845850dcc1660fab46883385691be75266cc95b9
 
 
 function App() {
   return (
 <>
+<<<<<<< HEAD
+<Navbar />
+=======
 {/* <Navbar /> */}
+>>>>>>> 845850dcc1660fab46883385691be75266cc95b9
     <BrowserRouter>
      <Routes>
         <Route exact path='/Home' element={<Home />} />
