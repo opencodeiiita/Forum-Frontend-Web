e4a1afc50ae0c790ad17f57869dce2e10e4dfd11
Vishal Ola <vishalola@Vishals-MacBook-Air.local>
2022-12-23 22:57:42 +0530

    Signin added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Hero from './components/hero-section';
-
+import SignIn from "./pages/signin";
 
 function App() {
   return (
 <>
-{/* <Navbar /> */}
+<Navbar />
     <BrowserRouter>
      <Routes>
         <Route exact path='/Home' element={<Home />} />
         <Route path='/About' element={<About />} />
         <Route path='/Contact' element={<Contact />} />
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+
       </Routes>
     </BrowserRouter>
 
-     <Hero/>
     </>
   );
 }
