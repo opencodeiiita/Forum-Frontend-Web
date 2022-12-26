f360a0b3076f91184b40d4466a7caa7d60fc1630
Ananya <goyalananya444@gmail.com>
2022-12-27 02:12:26 +0530

    error-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/hero-section";
+
+
 function Home() {
     return (
       <>
-        <h1 className='text-3xl font-bold text-center mt-10'>Home Page</h1>
+       <Navbar />
+       <Hero/>
+        {/* <h1 className='text-3xl font-bold text-center mt-10'>Home Page</h1> */}
       </>
     );
   }
