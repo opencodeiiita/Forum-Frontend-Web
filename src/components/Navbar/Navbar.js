ab4eb90fd159ec819c8e9ae039f40f91b69f5a5f
Kali-Decoder <neerajchoubisa876@gmail.com>
2023-01-05 10:02:19 +0530

    Update Nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,62 +1,75 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineSearch } from "react-icons/ai";
 import { FaConnectdevelop } from "react-icons/fa";
 import { BiMessage } from "react-icons/bi";
 export default function Navbar() {
   const btn = document.querySelector("button.mobile-menu-button");
   const menu = document.querySelector(".mobile-menu");
 
-  const [focusNav, setFocusNav] = useState(1);
   return (
-    <nav class="bg-gray-100">
-      <div class="max-w-6xl mx-auto px-4">
-        <div class="flex justify-between">
-          <div class="flex w-1/2 space-x-4 ">
-            <div className="">
-              <a
-                href="#"
-                class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+    <nav className="bg-gray-100">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex w-1/2 space-x-4 ">
+            <NavLink
+              to="/"
+              className=" items-center flex text-gray-700 hover:text-gray-900 mt-2"
+            >
+              <h1
+                className="font-bold text-2xl navbar_text"
+                style={{ color: "#02C8AC" }}
               >
-                <h1
-                  className="font-bold text-2xl navbar_text"
-                  style={{ color: "#02C8AC" }}
-                >
-                  Forum
-                </h1>
-              </a>
-            </div>
+                Forum
+              </h1>
+            </NavLink>
 
-            <div class="hidden md:flex items-center w-4/5">
-              <button
-                className="p-3 rounded-sm  mx-2 shadow-md focus:bg-green-400 "
-                style={{ borderRadius: "10px" }}
-              >
-                <AiOutlineHome fontSize="20px" />
-              </button>
-              <button
-                className="p-3 rounded-sm mx-2 shadow-md focus:bg-green-400 "
-                style={{ borderRadius: "10px" }}
-              >
-                <FaConnectdevelop fontSize="20px" />
-              </button>
-              <input
-                type="search"
-                id="search"
-                class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Type here to search ..."
-              />
-              <button
-                className="p-3 rounded-sm mx-2 bg-gray-200 focus:bg-green-400 "
-                style={{ borderRadius: "10px" }}
-              >
-                <BiMessage fontSize="20px" />
-              </button>
+            <div className="hidden md:flex items-center w-4/5 p-2">
+              <NavLink to="/">
+                <button
+                  className="p-3 rounded-sm  mx-2 shadow-md focus:bg-green-400 "
+                  style={{ borderRadius: "10px" }}
+                >
+                  {" "}
+                  <AiOutlineHome fontSize="20px" />
+                </button>
+              </NavLink>
+              <NavLink to="/community">
+                <button
+                  className="p-3 rounded-sm mx-2 shadow-md focus:bg-green-400 "
+                  style={{ borderRadius: "10px" }}
+                >
+                  {" "}
+                  <FaConnectdevelop fontSize="20px" />
+                </button>
+              </NavLink>
+              <div className="flex relative ">
+                <input
+                  type="search"
+                  id="search"
+                  className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Type here to search ..."
+                />
+                <AiOutlineSearch
+                  fontSize="25px"
+                  className="mt-3 absolute mx-1"
+                  style={{ right: "0%" }}
+                />
+              </div>
+              <NavLink to="/message">
+                <button
+                  className="p-3 rounded-sm mx-2 bg-gray-200 focus:bg-green-400 "
+                  style={{ borderRadius: "10px" }}
+                >
+                  {" "}
+                  <BiMessage fontSize="20px" />
+                </button>
+              </NavLink>
             </div>
           </div>
 
-          <div class="hidden md:flex items-center space-x-1">
+          <div className="hidden md:flex items-center space-x-1">
             <div
               className="profile w-8 h-8 rounded-full border overflow-hidden shadow-md"
               style={{ borderRadius: "10px" }}
@@ -69,24 +82,27 @@ export default function Navbar() {
             <h3 className="mx-4 mt-0 font-semibold ">Neeraj Coubisa</h3>
           </div>
 
-          <div class="md:hidden flex items-center">
+          <div className="md:hidden flex items-center">
+            <button>
+              <AiOutlineSearch fontSize="25px" className=" mx-2" />
+            </button>
             <button
-              class="mobile-menu-button"
+              className="mobile-menu-button"
               onClick={() => {
                 menu.classList.toggle("hidden");
               }}
             >
               <svg
-                class="w-6 h-6"
+                className="w-6 h-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
@@ -95,33 +111,30 @@ export default function Navbar() {
         </div>
       </div>
 
-      <div class="mobile-menu hidden md:hidden sm:flex flex-col p-2">
-        <button
-          className="p-3 rounded-sm  mx-2 mt-2 shadow-md focus:bg-green-400 "
-          style={{ borderRadius: "10px" }}
-        >
-          <AiOutlineHome fontSize="20px" />
-        </button>
-        <button
-          className="p-3 rounded-sm mx-2 mt-2 shadow-md focus:bg-green-400 "
-          style={{ borderRadius: "10px" }}
-        >
-          <FaConnectdevelop fontSize="20px" />
-        </button>
+      <div className="mobile-menu hidden md:hidden sm:flex flex-col p-2">
+        <div className="flex flex-col">
+          <button
+            className="p-3 rounded-sm  mt-2 shadow-md focus:bg-green-400 "
+            style={{ borderRadius: "10px" }}
+          >
+            <NavLink to="/">Home</NavLink>
+          </button>
+          <button
+            className="p-3 rounded-sm mt-2 shadow-md focus:bg-green-400 "
+            style={{ borderRadius: "10px" }}
+          >
+            <NavLink to="/community">Community</NavLink>
+          </button>
 
-        <button
-          className="p-3 rounded-sm mx-2 mt-2 bg-gray-200 focus:bg-green-400 "
-          style={{ borderRadius: "10px" }}
-        >
-          <BiMessage fontSize="20px" />
-        </button>
-        <input
-          type="search"
-          id="search"
-          class="bg-gray-100 mt-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Type here to search ..."
-        />
-        <div className="flex">
+          <button
+            className="p-3 rounded-sm mt-2 bg-gray-200 shadow-md focus:bg-green-400 "
+            style={{ borderRadius: "10px" }}
+          >
+            <NavLink to="/message">Message</NavLink>
+          </button>
+        </div>
+
+        <div className="flex mt-4">
           <div
             className="profile w-8 h-8 rounded-full border overflow-hidden shadow-md"
             style={{ borderRadius: "10px" }}
